24be2db99a2009b318c2c99b21beac328372a344
Pawcio1235 <166720156+Pawcio1235@users.noreply.github.com>
2024-05-09 11:56:59 +0200

    added map_book features

--- notatki.py
+++ notatki.py
@@ -1,11 +1,11 @@
 users: list[dict] = [
-    {'name': 'Jakub', 'surname': 'Orłowski', 'posts': 13},
+    {'name': 'Paweł', 'surname': 'Paszkowski', 'posts': 13},
     {'name': 'Janek', 'surname': 'Mielec', 'posts': 20},
     {'name': 'Maciej', 'surname': 'Przybytek', 'posts': 45},
     {'name': 'Bartosz', 'surname': 'Pietrasik', 'posts': 60},
     {'name': 'Tymoteusz', 'surname': 'Miszczak', 'posts': 21},
     {'name': 'Mateusz', 'surname': 'Matysiak', 'posts': 33},
-    {'name': 'Paweł', 'surname': 'Paszkowski', 'posts': 9},
+    {'name': 'Jakub', 'surname': 'Orłowski', 'posts': 9},
 ]
 
 
